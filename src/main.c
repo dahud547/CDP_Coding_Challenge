6bb0965fa0f18187a6f09955137690e6aa67c473
David Hudlow <dhudlow547@gmail.com>
2022-01-29 11:09:30 -0500

    Adding function to parse arguments

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <errno.h>
 
+int parse_arguments(const int argc, char * argv[], FILE * p_file);
 
 /**
  * @brief
@@ -10,6 +12,49 @@
  */
 int main(int argc, char * argv[])
 {
-    printf("I am a basic basic program, and I ran!\n");
-    return 0;
+    int ret_status = 0;
+    FILE * p_bin_file = NULL;
+
+    ret_status = parse_arguments(argc, argv, p_bin_file);
+
+    return ret_status;
+}
+
+/**
+ * @brief Function for parsing the inputs to the program
+ *
+ * @param argc Number of arguments being entered
+ * @param argv Array of the arguments
+ * @param p_file Output of the file being expected to be passed. Will be NULL if
+ *               invalid.
+ * @return int Return status, 0 for success, error code for failure
+ */
+int parse_arguments(const int argc, char * argv[], FILE * p_file)
+{
+    int ret_status = 0;
+    if (2 == argc)
+    {
+        p_file = fopen(argv[0], "rb");
+        if (NULL == p_file)
+        {
+            printf("ERR: Invalid File Location\n");
+            ret_status = ENODATA;
+        }
+        else
+        {
+            printf("File has been read in!\n");
+            (void)fclose(p_file);
+        }
+    }
+    else if (2 > argc)
+    {
+        printf("ERR: No imput file passed in!\n");
+        ret_status = ENODATA;
+    }
+    else
+    {
+        printf("ERR: This requires only 1 input bin file\n");
+        ret_status = -1;
+    }
+    return ret_status;
 }
