d1519d91116c1a5054e24bbd57ea880949b2455e
David Hudlow <dhudlow547@gmail.com>
2022-01-29 11:09:40 -0500

    Added a packet parser

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
+#include "packet_parser.h"
+
+#include <stdint.h>
 #include <stdio.h>
+#include <stddef.h>
 #include <errno.h>
 
 int parse_arguments(const int argc, char * argv[], FILE ** p_file);
+void get_pack_from_file(FILE ** p_file, uint8_t * out_buff, const size_t size);
 
 /**
  * @brief
@@ -17,11 +22,38 @@ int main(int argc, char * argv[])
 
     ret_status = parse_arguments(argc, argv, &p_bin_file);
 
-    while (0 == feof(p_bin_file))
+    pwr_packet_t pwr_pack = {0};
+    batt_packet_t batt_pack = {0};
+    while ((0 == feof(p_bin_file)) || (0 == ret_status))
     {
-        printf("I made it into the while loop\n");
-        break;
+        const uint8_t first_byte_of_pack = (uint8_t)fgetc(p_bin_file);
+        packet_type_t pack_type = determine_packet_type(first_byte_of_pack);
+
+        switch (pack_type)
+        {
+        case power_pack:
+        {
+            uint8_t temp_buf[SIZE_OF_PWR_PACK] = {0};
+            get_pack_from_file(&p_bin_file, temp_buf, SIZE_OF_PWR_PACK);
+            pwr_pack = process_pwr_packet(temp_buf);
+            break;
+        }
+        case battery_pack:
+        {
+            uint8_t temp_buf[SIZE_OF_BATT_PACK] = {0};
+            get_pack_from_file(&p_bin_file, temp_buf, SIZE_OF_BATT_PACK);
+            batt_pack = process_batt_packet(temp_buf);
+            break;
+        }
+        case error_type:
+            // Intentional fall-through
+        default:
+            ret_status = -1;
+            break;
+        }
     }
+
+    (void)fclose(p_bin_file);
 
     return ret_status;
 }
@@ -43,13 +75,8 @@ int parse_arguments(const int argc, char * argv[], FILE ** p_file)
         *p_file = fopen(argv[0], "rb");
         if (NULL == *p_file)
         {
-            printf("ERR: Invalid File Location\n");
-            ret_status = ENODATA;
-        }
-        else
-        {
-            printf("File has been read in!\n");
-            (void)fclose(*p_file);
+            perror("ERR: Invalid File Location\n");
+            ret_status = -1;
         }
     }
     else if (2 > argc)
@@ -63,4 +90,12 @@ int parse_arguments(const int argc, char * argv[], FILE ** p_file)
         ret_status = -1;
     }
     return ret_status;
+}
+
+void get_pack_from_file(FILE ** p_file, uint8_t * out_buff, const size_t size)
+{
+    for (size_t i = 0; size > i; ++i)
+    {
+        out_buff[0] = (uint8_t)fgetc(*p_file);
+    }
 }
---
+++ src/packet_parser.c
@@ -0,0 +1,112 @@
+/**
+ * @file packet_parser.c
+ *
+ * @brief Module for parsing the incoming packets
+ */
+
+#include "packet_parser.h"
+#include <stddef.h>
+
+#define START_OF_TS_LOC (0u)
+#define PWR_START_OF_VOLTS_LOC (START_OF_TS_LOC + TIMESTAMP_NUM_OF_BYTES)
+#define PWR_START_OF_MILLIAMP_LOC (PWR_START_OF_VOLTS_LOC + VOLTS_NUM_OF_BYTES)
+#define PWR_START_OF_ERR_CHECK_LOC (PWR_START_OF_MILLIAMP_LOC + MILLIAMP_NUM_OF_BYTES)
+#define BATT_START_OF_BATT_STATUS_LOC (START_OF_TS_LOC + TIMESTAMP_NUM_OF_BYTES)
+#define BATT_START_OF_ERR_CHECK_LOC (BATT_START_OF_BATT_STATUS_LOC + BATT_STAT_NUM_OF_BYTES)
+
+uint32_t convert_array_to_uint32(uint8_t * p_array, size_t length);
+
+/**
+ * @brief Function for determining the type of the incoming packet
+ *
+ * @param first_byte_of_pack 1st byte of the packet
+ * @return packet_type_t Type of packet, will return the Enum error_type if it's
+ *                       an invalid packet type.
+ */
+packet_type_t determine_packet_type(const uint8_t first_byte_of_pack)
+{
+    packet_type_t ret_type = error_type;
+    if (0u == first_byte_of_pack)
+    {
+        ret_type = power_pack;
+    }
+    else if (1u == first_byte_of_pack)
+    {
+        ret_type = battery_pack;
+    }
+    else
+    {
+        // Error so do nothing
+    }
+
+    return ret_type;
+}
+
+/**
+ * @brief Function for creating a power packet from an input buffer
+ *
+ * @param packet_buf Buffer to be converted
+ * @return pwr_packet_t Struct with the power information in it
+ */
+pwr_packet_t process_pwr_packet(uint8_t * packet_buf)
+{
+    pwr_packet_t ret_pack = {0};
+    if( NULL != packet_buf)
+    {
+        ret_pack.time_stamp = convert_array_to_uint32(&packet_buf[START_OF_TS_LOC],
+                                                      TIMESTAMP_NUM_OF_BYTES);
+
+        ret_pack.volts = convert_array_to_uint32(&packet_buf[PWR_START_OF_VOLTS_LOC],
+                                                 VOLTS_NUM_OF_BYTES);
+
+        ret_pack.milliamps = convert_array_to_uint32(&packet_buf[PWR_START_OF_MILLIAMP_LOC],
+                                                     MILLIAMP_NUM_OF_BYTES);
+
+        ret_pack.err_check = packet_buf[PWR_START_OF_ERR_CHECK_LOC];
+    }
+    return ret_pack;
+}
+
+/**
+ * @brief Function for creating a battery status packet from an input buffer
+ *
+ * @param packet_buf Buffer to be converted
+ * @return batt_packet_t Struct with the battery information in it.
+ */
+batt_packet_t process_batt_packet(uint8_t * packet_buf)
+{
+    batt_packet_t ret_pack = {0};
+
+    if( NULL != packet_buf)
+    {
+        ret_pack.time_stamp = convert_array_to_uint32(&packet_buf[START_OF_TS_LOC],
+                                                      TIMESTAMP_NUM_OF_BYTES);
+
+        ret_pack.batt_status = packet_buf[BATT_START_OF_BATT_STATUS_LOC];
+
+        ret_pack.err_check = packet_buf[BATT_START_OF_ERR_CHECK_LOC];
+    }
+
+    return ret_pack;
+}
+
+/**
+ * @brief Function for converting an uint8_t array to an uint32_t
+ *
+ * @todo assert that the length is <=4 bytes
+ *
+ * @param p_array Array to be converted
+ * @param length length of the array in bytes
+ * @return uint32_t
+ */
+uint32_t convert_array_to_uint32(uint8_t * p_array, size_t length)
+{
+    uint32_t ret_val = 0;
+    for (int i = (int)(length -1u); 0 <= i; --i)
+    {
+        ret_val <<= 8u;
+        ret_val += p_array[i];
+    }
+
+    return ret_val;
+}
