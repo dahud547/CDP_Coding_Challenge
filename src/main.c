eabd6058f3cf07146e60ba57c1f7739abbaa9240
David Hudlow <dhudlow547@gmail.com>
2022-01-29 11:10:54 -0500

    Adding some setup files and a very basic build system

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+
+/**
+ * @brief
+ *
+ * @param argc
+ * @param argv
+ * @return int
+ */
+int main(int argc, char * argv[])
+{
+    printf("I am a basic basic program, and I ran!\n");
+    return 0;
+}
